a56573d69f4859738daf4b9ae014b45424a6c720
IagoVieiraa <iago.vieiras13@gmail.com>
2025-08-10 11:40:15 -0300

    adicionando teste para criação de usuário com email repetido

--- auth-service/app/tests/test_user.py
+++ auth-service/app/tests/test_user.py
@@ -3,8 +3,13 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_create_user():
+def test_create_user_returns_201():
     response = client.post("/auth/users/create", json={"email": "teste8@exemplo.com", "password": "123456"})
     print(response.json())
     assert response.status_code == 201
     assert "id" in response.json()
+
+def test_create_user_when_email_already_exists_returns_400():
+    response = client.post("/auth/users/create", json={"email": "teste8@exemplo.com", "password": "123456"})
+    assert response.status_code == 400
+    assert "Email already exists" in response.json()["body"]
