da4c10d6e11ff2b5fbd30cd0fd55580d42d339c8
IagoVieiraa <iago.vieiras13@gmail.com>
2025-08-27 14:03:49 -0300

    ajuste na rota de criar pedido

--- order-service/app/api/order_route.py
+++ order-service/app/api/order_route.py
@@ -6,15 +6,9 @@ from ..schemas.order_schema import Order
 router = APIRouter()
 
 @router.post("/create")
-def create_order(order_data: dict, authorization: str = Header(None)):
-    # if authorization and authorization.startswith("Bearer "):
-    token = order_data.get("user_token")
-    result = order_service.create_order(order_data, token)
-        # Aqui você pode associar o pedido ao user_info["user_id"]
-    # else:
-    #     user_info = None  # Pedido anônimo
+def create_order(order_data: Order, authorization: str = Header(None)):
+    result = order_service.create_order(order_data, authorization)
 
-    # Criação do pedido (com ou sem usuário vinculado)
     return JSONResponse(
         content=result,
         status_code=result["status_code"]
